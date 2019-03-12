30ee1c3f970023692c243aed7a5984b10bbd1d11
witt <mymeek@163.com>
2019-03-12 10:31:06 +0800

    simple example

--- src/main/java/com/witt/beans/User.java
+++ src/main/java/com/witt/beans/User.java
@@ -1,7 +1,5 @@
 package com.witt.beans;
 
-import lombok.Data;
-
 /**
  * @author witt
  * @fileName User
@@ -11,8 +9,39 @@ import lombok.Data;
  * @history <author>    <time>    <version>    <desc>
  */
 
-@Data
 public class User {
     private Integer id;
     private String name;
+
+    public User() {
+    }
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static User of(String name) {
+        return new User(name);
+    }
 }
--- src/main/java/com/witt/velocity/ExampleVelocity.java
+++ src/main/java/com/witt/velocity/ExampleVelocity.java
@@ -1,5 +1,7 @@
 package com.witt.velocity;
 
+import com.witt.beans.User;
+
 import java.util.*;
 
 /**
@@ -17,54 +19,8 @@ public class ExampleVelocity {
         return "show velocity";
     }
 
-    public List<String> list() {
-        return Arrays.asList("dior", "lv", "boos", "Gucci");
+    public List<User> list() {
+        return Arrays.asList(User.of("dior"), User.of("Gucci"), User.of("boos"));
     }
 
-    public String whether(boolean whether) {
-        if (whether) return "is true";
-        return "is false";
-    }
-
-    public Map<Integer, Pair> map() {
-        Map<Integer, Pair> map = new HashMap<>();
-        Pair pair1 = Pair.of("a", "A");
-        Pair pair2 = Pair.of("b", "B");
-        Pair pair3 = Pair.of("c", "C");
-        map.put(1, pair1);
-        map.put(2, pair2);
-        map.put(3, pair3);
-        return map;
-    }
-
-}
-
-class Pair<A, B> {
-    public final A fst;
-    public final B snd;
-
-    public Pair(A var1, B var2) {
-        this.fst = var1;
-        this.snd = var2;
-    }
-
-    public String toString() {
-        return "Pair[" + this.fst + "," + this.snd + "]";
-    }
-
-    public boolean equals(Object var1) {
-        return var1 instanceof Pair && Objects.equals(this.fst, ((Pair)var1).fst) && Objects.equals(this.snd, ((Pair)var1).snd);
-    }
-
-    public int hashCode() {
-        if (this.fst == null) {
-            return this.snd == null ? 0 : this.snd.hashCode() + 1;
-        } else {
-            return this.snd == null ? this.fst.hashCode() + 2 : this.fst.hashCode() * 17 + this.snd.hashCode();
-        }
-    }
-
-    public static <A, B> Pair<A, B> of(A var0, B var1) {
-        return new Pair(var0, var1);
-    }
 }
